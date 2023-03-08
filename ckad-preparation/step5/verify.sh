ca17a8e7734dc397d45d0d73bd0fcff3b8bba236
SandyK8 <127329153+SandyK8@users.noreply.github.com>
2023-03-08 18:36:51 -0400

    firstPR

---
+++ ckad-preparation/step5/verify.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -f /opt/course/skips/pregunta4.txt ]; then
+    echo "Ejecución correcta."
+    # Notificar al server resultado
+    archivo=$(cat /opt/course/team/equipo.txt)
+    json="{\"team\":\"$archivo\", \"question\":4, \"result\":\"skip\"}"
+    curl -X POST -H "Content-Type: application/json" -d "$json" https://api-dev.bhd.com.do/killer-coda/result >/dev/null 2>&1 &
+else
+    if [ ! -f /opt/course/1/namespaces.txt ]; then
+        echo "Error: no existe el archivo /opt/course/1/namespaces.txt."
+        exit 1
+    fi
+    echo "Ejecución correcta."
+
+    # Notificar al server resultado
+    archivo=$(cat /opt/course/team/equipo.txt)
+    json="{\"team\":\"$archivo\", \"question\":4, \"result\":\"success\"}"
+    curl -X POST -H "Content-Type: application/json" -d "$json" https://api-dev.bhd.com.do/killer-coda/result >/dev/null 2>&1 &
+
+fi
